78fdc587918f64647f32e48bf91d600e3cafbc9c
André Apitzsch <andre.apitzsch@etit.tu-chemnitz.de>
2023-06-16 13:20:06 +0200

    fd-net-device,tap-bridge: Unify tap-creator and tap-device-creator

--- src/tap-bridge/model/tap-bridge.cc
+++ src/tap-bridge/model/tap-bridge.cc
@@ -500,7 +500,7 @@ TapBridge::CreateTap()
         NS_LOG_DEBUG("Executing: " << TAP_CREATOR << " " << ossDeviceName.str() << " "
                                    << ossIp.str() << " " << ossMac.str() << " " << ossNetmask.str()
                                    << " " << ossMode.str() << " " << ossPath.str() << " "
-                                   << ossVerbose.str());
+                                   << ossVerbose.str() << " -t");
 
         //
         // Execute the socket creation process image.
@@ -514,6 +514,7 @@ TapBridge::CreateTap()
                           ossMode.str().c_str(),       // argv[5] (-o<operating mode>)
                           ossPath.str().c_str(),       // argv[6] (-p<path>)
                           ossVerbose.str().c_str(),    // argv[7] (-v)
+                          "-t",                        // argv[8] (-t <tap>)
                           (char*)nullptr);
 
         //
