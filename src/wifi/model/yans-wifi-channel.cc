shreyasgowda63/NS_3

wifi: Convert a few attributes to DecibelValue

src/wifi/model/yans-wifi-channel.cc
@@ -134,7 +134,7 @@ void
 YansWifiChannel::Receive(Ptr<YansWifiPhy> phy, Ptr<const WifiPpdu> ppdu, double rxPowerDbm)
 {
     NS_LOG_FUNCTION(phy << ppdu << rxPowerDbm);
-    const auto totalRxPowerDbm = rxPowerDbm + phy->GetRxGain();
+    const auto totalRxPowerDbm = rxPowerDbm + phy->GetRxGain().to<double>();
     phy->TraceSignalArrival(ppdu, totalRxPowerDbm, ppdu->GetTxDuration());
     // Do no further processing if signal is too weak
     // Current implementation assumes constant RX power over the PPDU duration

sanchuanhehe/ns-3-dev

src/wifi/model/yans-wifi-channel.cc
@@ -123,7 +123,7 @@ void
 YansWifiChannel::Receive(Ptr<YansWifiPhy> phy, Ptr<const WifiPpdu> ppdu, dBm_u rxPower)
 {
     NS_LOG_FUNCTION(phy << ppdu << rxPower);
-    const auto totalRxPower = rxPower + phy->GetRxGain();
+    const auto totalRxPower = rxPower + phy->GetRxGain().to<double>();
     phy->TraceSignalArrival(ppdu, totalRxPower, ppdu->GetTxDuration());
     // Do no further processing if signal is too weak
     // Current implementation assumes constant RX power over the PPDU duration