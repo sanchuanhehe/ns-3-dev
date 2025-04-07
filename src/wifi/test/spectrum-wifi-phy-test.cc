shreyasgowda63/NS_3

Make some wifi code use the dBm_t type

src/wifi/test/spectrum-wifi-phy-test.cc
@@ -1865,8 +1865,8 @@ SpectrumWifiPhyMultipleInterfacesTest::SendPpdu(Ptr<SpectrumWifiPhy> phy,
     m_lastTxEnd = m_lastTxStart + WifiPhy::CalculateTxDuration({std::make_pair(SU_STA_ID, psdu)},
                                                                txVector,
                                                                phy->GetPhyBand());
-    phy->SetTxPowerStart(txPowerDbm);
-    phy->SetTxPowerEnd(txPowerDbm);
+    phy->SetTxPowerStart(DBm(txPowerDbm));
+    phy->SetTxPowerEnd(DBm(txPowerDbm));
     phy->Send(WifiConstPsduMap({std::make_pair(SU_STA_ID, psdu)}), txVector);
 }
 

src/wifi/test/wifi-non-ht-dup-test.cc
@@ -928,8 +928,8 @@ TestMultipleCtsResponsesFromMuRts::DoSetup()
         phySta->AddChannel(spectrumChannel);
         phySta->ConfigureStandard(WIFI_STANDARD_80211ax);
         phySta->AssignStreams(streamNumber);
-        phySta->SetTxPowerStart(m_stasTxPowerDbm);
-        phySta->SetTxPowerEnd(m_stasTxPowerDbm);
+        phySta->SetTxPowerStart(DBm(m_stasTxPowerDbm));
+        phySta->SetTxPowerEnd(DBm(m_stasTxPowerDbm));
 
         auto channelNum = WifiPhyOperatingChannel::FindFirst(0,
                                                              0,

src/wifi/test/wifi-phy-cca-test.cc
@@ -889,8 +889,8 @@ WifiPhyCcaIndicationTest::SendHeSuPpdu(double txPowerDbm,
     hdr.SetQosTid(0);
     Ptr<WifiPsdu> psdu = Create<WifiPsdu>(pkt, hdr);
 
-    m_txPhy->SetTxPowerStart(txPowerDbm);
-    m_txPhy->SetTxPowerEnd(txPowerDbm);
+    m_txPhy->SetTxPowerStart(units::power::dBm_t(txPowerDbm));
+    m_txPhy->SetTxPowerEnd(units::power::dBm_t(txPowerDbm));
 
     m_txPhy->Send(psdu, txVector);
 }

sanchuanhehe/ns-3-dev

src/wifi/test/spectrum-wifi-phy-test.cc
@@ -1840,8 +1840,8 @@ SpectrumWifiPhyMultipleInterfacesTest::SendPpdu(Ptr<SpectrumWifiPhy> phy,
     m_lastTxEnd = m_lastTxStart + WifiPhy::CalculateTxDuration({std::make_pair(SU_STA_ID, psdu)},
                                                                txVector,
                                                                phy->GetPhyBand());
-    phy->SetTxPowerStart(txPower);
-    phy->SetTxPowerEnd(txPower);
+    phy->SetTxPowerStart(DBm(txPower));
+    phy->SetTxPowerEnd(DBm(txPower));
     phy->Send(WifiConstPsduMap({std::make_pair(SU_STA_ID, psdu)}), txVector);
 }
 

src/wifi/test/wifi-non-ht-dup-test.cc
@@ -918,8 +918,8 @@ TestMultipleCtsResponsesFromMuRts::DoSetup()
         phySta->AddChannel(spectrumChannel);
         phySta->ConfigureStandard(WIFI_STANDARD_80211ax);
         phySta->AssignStreams(streamNumber);
-        phySta->SetTxPowerStart(m_stasTxPower);
-        phySta->SetTxPowerEnd(m_stasTxPower);
+        phySta->SetTxPowerStart(DBm(m_stasTxPower));
+        phySta->SetTxPowerEnd(DBm(m_stasTxPower));
 
         auto channelNum = WifiPhyOperatingChannel::FindFirst(0,
                                                              MHz_u{0},

src/wifi/test/wifi-phy-cca-test.cc
@@ -908,8 +908,8 @@ WifiPhyCcaIndicationTest::SendHeSuPpdu(dBm_u txPower, MHz_u frequency, MHz_u ban
     hdr.SetQosTid(0);
     Ptr<WifiPsdu> psdu = Create<WifiPsdu>(pkt, hdr);
 
-    m_txPhy->SetTxPowerStart(txPower);
-    m_txPhy->SetTxPowerEnd(txPower);
+    m_txPhy->SetTxPowerStart(units::power::dBm_t(txPower));
+    m_txPhy->SetTxPowerEnd(units::power::dBm_t(txPower));
 
     m_txPhy->Send(psdu, txVector);
 }