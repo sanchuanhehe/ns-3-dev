nsnam/ns-3-dev-git

wifi: Handle CCA for 802.11be PHY

src/wifi/model/eht/eht-configuration.h
@@ -110,7 +110,8 @@ class EhtConfiguration : public Object
     std::map<std::list<uint64_t>, std::list<uint64_t>>
         m_linkMappingDl; //!< TIDs-indexed Link Mapping for downlink
     std::map<std::list<uint64_t>, std::list<uint64_t>>
-        m_linkMappingUl; //!< TIDs-indexed Link Mapping for uplink
+        m_linkMappingUl;                  //!< TIDs-indexed Link Mapping for uplink
+    dBm_u m_per20CcaSensitivityThreshold; //!< CCA threshold for Per 20MHz check
 };
 
 } // namespace ns3

sanchuanhehe/ns-3-dev

src/wifi/model/eht/eht-configuration.h
@@ -110,7 +110,8 @@ class EhtConfiguration : public Object
     std::map<std::list<uint64_t>, std::list<uint64_t>>
         m_linkMappingDl; //!< TIDs-indexed Link Mapping for downlink
     std::map<std::list<uint64_t>, std::list<uint64_t>>
-        m_linkMappingUl; //!< TIDs-indexed Link Mapping for uplink
+        m_linkMappingUl;                 //!< TIDs-indexed Link Mapping for uplink
+    dB_u m_per20CcaSensitivityThreshold; //!< CCA threshold for Per 20MHz check
 };
 
 } // namespace ns3